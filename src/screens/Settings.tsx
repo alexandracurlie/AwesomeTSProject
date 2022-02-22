bc2c817aa33dff3cc9c204be3a36d3fa2fa06ca2
Alexandra <alexa.curlie@gmail.com>
2022-02-22 10:54:41 +0300

    Feat: New components ModalInfo, ModalForm, Button.Added functionality to components TransactionForm and AddCard. refactor: code simplified, useless constants removed.

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import {SafeAreaView, StyleSheet, ScrollView, View} from 'react-native';
+import {SafeAreaView, StyleSheet, FlatList} from 'react-native';
 import {Header} from "../components/Header/Header";
 import {Card} from "../components/Cards/Card";
 import {cardsData} from "../utilities";
-import {Colors} from "../styles";
+import {Colors, ScreenSize} from "../styles";
 
-export const Settings = () => (
-  <SafeAreaView style={styles.safeArea}>
-    <ScrollView>
+export const Settings = () => {
+  const renderItem = ({item}: any) => <Card title={item.title} icon={item.icon} key={item.id} />
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
       <Header content={"Settings"} />
-      <View style={styles.list}>
-        {cardsData.map(item => <Card title={item.title}
-                                     icon={item.icon}
-                                     key={item.id}/>)}
-      </View>
-    </ScrollView>
-  </SafeAreaView>
-);
+      <FlatList data={cardsData}
+                renderItem={renderItem}
+                style={styles.list}
+                contentContainerStyle={styles.contentContainerStyle}
+                numColumns={2}/>
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   safeArea: {
@@ -24,9 +26,16 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
   },
 
+  contentContainerStyle: {
+    justifyContent: "space-between",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+
   list: {
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "space-evenly",
+    width: ScreenSize.width * 100,
   }
 });
